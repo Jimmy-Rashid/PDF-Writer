aa1b13530d921b2b908c3a3f30bbc3c5a4c2fc59
Jimmy Rashid <jimmyrashid22@gmail.com>
2024-03-26 14:17:05 -0700

    Working field entry

--- fill_form.py
+++ fill_form.py
@@ -1,29 +1,32 @@
 from pypdf import PdfReader, PdfWriter
 
 date = "Test Date"
+city = "Test City"
 owner = "Test Owner"
 zoning = "Test Zoning"
-# address = "Test Address"
+address = "Test Address"
 dimensions = "Test Dimensions"
 floor_area = "Test Floor Area"
 
 input_file = "Burnaby Template - Form.pdf"
 # output_file = str(address) + " - Property Report.pdf"
-output_file = " - Property Report.pdf"
+output_file = "Property Report.pdf"
 
 reader = PdfReader(input_file)
-# print(reader.get_fields().keys()) # prints the fields in the pdf
+# print(reader.get_fields().keys())  # prints the fields in the pdf
 
 writer = PdfWriter()
 writer.append(reader)
 
 fields = {
     "date": date,
+    "city": city,
     "owner": owner,
     "zoning": zoning,
-    # "address": address,
+    "address_1": address,
+    "address_2": address,
     "dimensions": dimensions,
-    # "floor_area": floor_area,
+    "floor_area": floor_area,
 }
 
 keys = fields.keys()
@@ -38,3 +41,7 @@ for count in range(len(reader.pages)):
 
 with open(output_file, "wb") as output_stream:
     writer.write(output_stream)
+
+# # prints keys and values in the dict
+# for x in range(7):
+#     print(f"{list(keys)[x]}"+": "+f"{fields[list(keys)[x]]}")
